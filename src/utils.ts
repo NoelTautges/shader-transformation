54fea57094ccaad3b3dec5e07e305a55bf199759
Noel Tautges <noelatautges@gmail.com>
2022-01-05 00:57:01 -0600

    Add attribution for Cartesian product function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+// Credit to rsp on StackOverflow under CC BY-SA 4.0: https://stackoverflow.com/a/43053803
 const cartesian =
   (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
 
